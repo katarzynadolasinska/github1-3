69acc27b44d6f35241cbc676b48b37d05bd64b3f
katarzynadolasinska <katarzynadolasinskaxox@gmail.com>
2023-09-02 22:00:53 +0200

    changed text

--- homework/homework/Program.cs
+++ homework/homework/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,!");
+            Console.WriteLine(" something else than hello world");
         }
     }
 }
